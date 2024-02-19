560d2df98736bc6d4c9a56efbd3f45dc6616884b
Stanley Wintergreen <kepocnhh@gmail.com>
2024-02-19 15:49:14 +0300

    minSdk:28

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -4,7 +4,7 @@ object Version {
 
     object Android {
         const val compileSdk = 34
-        const val minSdk = 24
+        const val minSdk = 28
         const val targetSdk = compileSdk
         const val compose = "1.5.4" // kotlin 1.9.20
     }
