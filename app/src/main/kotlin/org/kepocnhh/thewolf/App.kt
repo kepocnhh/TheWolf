972004309bcc5e505dd4750ddc79ec55d62e4fdd
Stanley Wintergreen <kepocnhh@gmail.com>
2024-02-19 15:53:49 +0300

    app composition

--- app/src/main/kotlin/org/kepocnhh/thewolf/App.kt
+++ app/src/main/kotlin/org/kepocnhh/thewolf/App.kt
@@ -1,8 +1,22 @@
 package org.kepocnhh.thewolf
 
 import android.app.Application
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 
 internal class App : Application() {
+    object Theme {
+        @Composable
+        fun Composition(
+            content: @Composable () -> Unit,
+        ) {
+            // todo
+            CompositionLocalProvider(
+                content = content,
+            )
+        }
+    }
+
     override fun onCreate() {
         super.onCreate()
         // todo
--- app/src/main/kotlin/org/kepocnhh/thewolf/MainActivity.kt
+++ app/src/main/kotlin/org/kepocnhh/thewolf/MainActivity.kt
@@ -1,11 +1,20 @@
 package org.kepocnhh.thewolf
 
 import android.os.Bundle
+import androidx.activity.compose.BackHandler
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 
 internal class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // todo
+        setContent {
+            BackHandler {
+                finish()
+            }
+            App.Theme.Composition {
+                // todo
+            }
+        }
     }
 }
