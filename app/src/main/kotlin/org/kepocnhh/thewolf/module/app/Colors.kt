0cf456b9bd92880b436b56560dacc0ab064cf33e
Stanley Wintergreen <kepocnhh@gmail.com>
2024-03-01 17:46:37 +0300

    column

--- app/src/main/kotlin/org/kepocnhh/thewolf/module/app/Colors.kt
+++ app/src/main/kotlin/org/kepocnhh/thewolf/module/app/Colors.kt
@@ -4,16 +4,22 @@ import androidx.compose.ui.graphics.Color
 
 internal class Colors private constructor(
     val background: Color,
+    val secondary: Color,
     val foreground: Color,
+    val text: Color,
 ) {
     companion object {
         val Dark = Colors(
-            background = Color(0xff000000),
-            foreground = Color(0xffffffff),
+            background = Color(0xff1f1f1f),
+            secondary = Color(0xff2f2f2f),
+            foreground = Color(0xffefefef),
+            text = Color(0xffffffff),
         )
         val Light = Colors(
-            background = Color(0xffffffff),
-            foreground = Color(0xff000000),
+            background = Color(0xffefefef),
+            secondary = Color(0xffdfdfdf),
+            foreground = Color(0xff1f1f1f),
+            text = Color(0xff000000),
         )
     }
 }
--- app/src/main/kotlin/org/kepocnhh/thewolf/module/tasks/TasksScreen.kt
+++ app/src/main/kotlin/org/kepocnhh/thewolf/module/tasks/TasksScreen.kt
@@ -1,29 +1,36 @@
 package org.kepocnhh.thewolf.module.tasks
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.kepocnhh.thewolf.App
+import org.kepocnhh.thewolf.entity.Task
 
 @Composable
 private fun Text(text: String) {
     val textStyle = TextStyle(
-        color = App.Theme.colors.foreground,
+        color = App.Theme.colors.text,
         textAlign = TextAlign.Start,
     )
     BasicText(
@@ -37,26 +44,62 @@ private fun Text(text: String) {
 }
 
 @Composable
+private fun TaskItem(item: Task) {
+    Box(
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .background(App.Theme.colors.secondary, RoundedCornerShape(32.dp))
+            .fillMaxWidth()
+            .height(64.dp),
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 32.dp),
+        ) {
+            BasicText(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentHeight(),
+                text = item.title,
+                style = TextStyle(
+                    color = App.Theme.colors.text,
+                    textAlign = TextAlign.Start,
+                    fontSize = 17.sp,
+                ),
+                overflow = TextOverflow.Ellipsis,
+                minLines = 1,
+                maxLines = 1,
+            )
+        }
+    }
+}
+
+@Composable
 private fun TasksScreen(state: TasksLogics.State) {
     Box(
         modifier = Modifier
             .fillMaxSize(),
     ) {
         val insets = App.Theme.insets
+        val verticalPadding = 16.dp
         val contentPadding = PaddingValues(
-            top = insets.calculateTopPadding(),
-            bottom = insets.calculateBottomPadding(),
+            top = insets.calculateTopPadding() + verticalPadding,
+            bottom = insets.calculateBottomPadding() + verticalPadding,
         )
+        val itemsPadding = 16.dp
+        val itemsAlign = Alignment.CenterVertically
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize(),
             contentPadding = contentPadding,
+            verticalArrangement = Arrangement.spacedBy(itemsPadding, itemsAlign),
         ) {
             items(
                 items = state.tasks,
                 key = { it.id },
             ) { task ->
-                Text(text = task.title)
+                TaskItem(item = task)
             }
         }
     }
