0823c7ade6a696c0391949f0c421f12736c31589
Stanley Wintergreen <kepocnhh@gmail.com>
2024-03-08 16:05:40 +0300

    com.github.kepocnhh:ComposeAnimations:0.1.0u-SNAPSHOT

--- app/src/main/kotlin/org/kepocnhh/thewolf/module/tasks/NewTaskScreen.kt
+++ app/src/main/kotlin/org/kepocnhh/thewolf/module/tasks/NewTaskScreen.kt
@@ -1,23 +1,17 @@
 package org.kepocnhh.thewolf.module.tasks
 
-import androidx.compose.animation.AnimatedVisibilityScope
-import androidx.compose.animation.core.Easing
 import androidx.compose.foundation.background
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -25,24 +19,16 @@ import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.platform.LocalTextInputService
-import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.kepocnhh.thewolf.App
 import org.kepocnhh.thewolf.util.compose.BackHandler
 import org.kepocnhh.thewolf.util.compose.plus
-import sp.ax.jc.animations.style.LocalTweenStyle
-import sp.ax.jc.animations.tween.fade.FadeVisibility
-import sp.ax.jc.animations.tween.slide.SlideHVisibility
-import sp.ax.jc.animations.tween.slide.SlideVisibility
+import sp.ax.jc.animations.tween.slide.vertical.SlideVVisibility
 import sp.ax.jc.keyboard.Keyboard
-import java.lang.StringBuilder
-import kotlin.time.Duration
 
 @Composable
 private fun TextField(
@@ -103,31 +89,6 @@ private fun TextField(text: String) {
 }
 
 @Composable
-private fun SlideVVisibility(
-    visible: Boolean,
-    modifier: Modifier = Modifier,
-    label: String = "SlideHVisibility",
-    duration: Duration = LocalTweenStyle.current.duration,
-    delay: Duration = LocalTweenStyle.current.delay,
-    easing: Easing = LocalTweenStyle.current.easing,
-    initialOffsetY: (fullHeight: Int) -> Int = { it },
-    targetOffsetY: (fullHeight: Int) -> Int = { it },
-    content: @Composable AnimatedVisibilityScope.() -> Unit,
-) {
-    SlideVisibility(
-        visible = visible,
-        modifier = modifier,
-        label = label,
-        duration = duration,
-        delay = delay,
-        easing = easing,
-        initialOffset = { IntOffset(x = 0, y = initialOffsetY(it.height)) },
-        targetOffset = { IntOffset(x = 0, y = targetOffsetY(it.height)) },
-        content = content,
-    )
-}
-
-@Composable
 internal fun NewTaskScreen(
     onBack: () -> Unit,
     onNewTask: (title: String) -> Unit,
@@ -178,15 +139,6 @@ internal fun NewTaskScreen(
                     },
                 )
             }
-//            FadeVisibility(visible = isFocusedState.value) {
-//                Keyboard(
-//                    modifier = Modifier
-//                        .fillMaxWidth(),
-//                    onClick = { char ->
-//                        textFieldValueState.value += char
-//                    },
-//                )
-//            }
         }
     }
 }
