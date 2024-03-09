50ba7f8daf03352d3acd89943dda0feaf339617c
Stanley Wintergreen <kepocnhh@gmail.com>
2024-03-09 16:25:56 +0300

    circle button

--- app/src/main/kotlin/org/kepocnhh/thewolf/module/app/Colors.kt
+++ app/src/main/kotlin/org/kepocnhh/thewolf/module/app/Colors.kt
@@ -9,23 +9,28 @@ internal class Colors private constructor(
     val foreground: Color,
     val text: Color,
     val primary: Color,
+    val icon: Color,
 ) {
     companion object {
+        val White = Color(0xffffffff)
+
         val Dark = Colors(
             basement = Color(0xff0f0f0f),
             background = Color(0xff1f1f1f),
             secondary = Color(0xff2f2f2f),
             foreground = Color(0xffefefef),
-            text = Color(0xffffffff),
+            text = White,
             primary = Color(0xff1E88E5),
+            icon = White,
         )
         val Light = Colors(
-            basement = Color(0xffffffff),
+            basement = White,
             background = Color(0xffefefef),
             secondary = Color(0xffdfdfdf),
             foreground = Color(0xff1f1f1f),
             text = Color(0xff000000),
             primary = Color(0xff1E88E5),
+            icon = Color(0xff000000),
         )
     }
 }
--- app/src/main/kotlin/org/kepocnhh/thewolf/module/tasks/TasksScreen.kt
+++ app/src/main/kotlin/org/kepocnhh/thewolf/module/tasks/TasksScreen.kt
@@ -1,16 +1,28 @@
 package org.kepocnhh.thewolf.module.tasks
 
+import androidx.annotation.DrawableRes
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.Indication
+import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -19,19 +31,30 @@ import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.DefaultShadowColor
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.kepocnhh.thewolf.App
+import org.kepocnhh.thewolf.R
 import org.kepocnhh.thewolf.entity.Task
+import org.kepocnhh.thewolf.module.app.Colors
+import org.kepocnhh.thewolf.util.compose.ColorIndication
 import sp.ax.jc.animations.tween.fade.FadeVisibility
 import sp.ax.jc.animations.tween.slide.SlideHVisibility
 import sp.ax.jc.squares.Squares
@@ -77,6 +100,7 @@ private fun TaskItem(item: Task) {
 private fun TasksScreen(
     state: TasksLogics.State,
     onNewTask: () -> Unit,
+    onSettings: () -> Unit,
 ) {
     Box(
         modifier = Modifier
@@ -103,21 +127,88 @@ private fun TasksScreen(
                 TaskItem(item = task)
             }
         }
-        BasicText(
+        Row(
             modifier = Modifier
-                .padding(
-                    bottom = insets.calculateBottomPadding() + 8.dp,
-                    end = insets.calculateEndPadding(LayoutDirection.Ltr) + 8.dp
+                .fillMaxWidth()
+                .padding(bottom = insets.calculateBottomPadding())
+                .padding(bottom = 16.dp)
+                .padding(horizontal = 16.dp)
+                .align(Alignment.BottomEnd),
+        ) {
+            CircleButton(
+                color = App.Theme.colors.secondary,
+                iconColor = App.Theme.colors.icon,
+                iconId = R.drawable.gear,
+                contentDescription = "TasksScreen:settings",
+                onClick = onSettings,
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            CircleButton(
+                indication = remember { ColorIndication.create(Colors.White) },
+                color = App.Theme.colors.primary,
+                iconColor = Colors.White,
+                iconId = R.drawable.plus,
+                contentDescription = "TasksScreen:new:task",
+                onClick = onNewTask,
+            )
+        }
+    }
+}
+
+@Composable
+private fun CircleButton(
+    enabled: Boolean = true,
+    indication: Indication = LocalIndication.current,
+    elevation: Dp = 8.dp,
+    size: Dp = 64.dp,
+    color: Color,
+    iconSize: Dp = 24.dp,
+    iconColor: Color,
+    @DrawableRes iconId: Int,
+    contentDescription: String,
+    onClick: () -> Unit,
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val onClickState = rememberUpdatedState(onClick)
+    val elevationTarget = remember { mutableFloatStateOf(elevation.value) }
+    val elevationActual = animateFloatAsState(
+        targetValue = elevationTarget.floatValue,
+        label = "CircleButton:$contentDescription",
+    ).value.dp
+    Box(
+        modifier = Modifier
+            .size(size)
+            .shadow(
+                elevation = elevationActual,
+                shape = RoundedCornerShape(size / 2),
+            )
+            .background(color, RoundedCornerShape(size / 2))
+            .indication(interactionSource, indication)
+            .pointerInput(interactionSource, enabled) {
+                detectTapGestures(
+                    onPress = { offset ->
+                        val press = PressInteraction.Press(offset)
+                        interactionSource.emit(press)
+                        elevationTarget.floatValue = elevation.value / 2
+                        if (tryAwaitRelease()) {
+                            interactionSource.emit(PressInteraction.Release(press))
+                            if (enabled) onClickState.value()
+                        } else {
+                            interactionSource.emit(PressInteraction.Cancel(press))
+                        }
+                        elevationTarget.floatValue = elevation.value
+                    },
                 )
-                .background(Color.Black)
-                .align(Alignment.BottomEnd)
-                .clickable(onClick = onNewTask)
-                .padding(8.dp),
-            text = "new task",
-            style = TextStyle(
-                color = Color.White,
-            ),
-        ) // todo
+            },
+    ) {
+        Image(
+            modifier = Modifier
+                .size(iconSize)
+                .align(Alignment.Center),
+            painter = painterResource(id = iconId),
+            contentDescription = contentDescription,
+            colorFilter = ColorFilter.tint(iconColor),
+        )
     }
 }
 
@@ -140,11 +231,16 @@ internal fun TasksScreen() {
                 onNewTask = {
                     newTaskState.value = true
                 },
+                onSettings = {
+                    // todo
+                }
             )
             FadeVisibility(
                 visible = newTaskState.value,
             ) {
-                Box(modifier = Modifier.fillMaxSize().background(Color.Black.copy(alpha = 0.75f)))
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Black.copy(alpha = 0.75f)))
             }
             SlideHVisibility(
                 visible = newTaskState.value,
